c46473280908385346bbc20dbafc9b644a6e8f72
GhencianTimotei <74396814+GhencianTimotei@users.noreply.github.com>
2022-01-08 14:24:48 +0200

    Update

--- HTTPOnly/javascript/sidebars.js
+++
@@ -1,4 +0,0 @@
-var dropDown = document.getElementById("dropdownUser1");
-dropDown.addEventListener('click', ev => {
-  document.getElementById("dropdown-menu").classList.toggle("dropdown-menu_show");
-});
